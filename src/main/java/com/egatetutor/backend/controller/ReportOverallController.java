790672300cff4c8c16893f01f34ec4d67cb03054
Honey <honeygoyal1510@gmail.com>
2020-08-26 18:02:04 +0530

    Set total marks in test analytics!

--- src/main/java/com/egatetutor/backend/controller/ReportOverallController.java
+++ src/main/java/com/egatetutor/backend/controller/ReportOverallController.java
@@ -4,24 +4,18 @@ package com.egatetutor.backend.controller;
 import com.egatetutor.backend.enumType.CoursesStatus;
 import com.egatetutor.backend.model.*;
 import com.egatetutor.backend.model.compositekey.ReportOverallPK;
-import com.egatetutor.backend.model.responsemodel.CourseDescStatusResponse;
 import com.egatetutor.backend.model.responsemodel.ReportOverallRequest;
 import com.egatetutor.backend.model.responsemodel.TestAnalytics;
 import com.egatetutor.backend.repository.*;
 import com.egatetutor.backend.service.ReportGeneratorService;
-import org.aspectj.weaver.ast.Test;
-import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/reportOverall")
@@ -53,6 +47,11 @@ public class ReportOverallController {
             throw new Exception("Exam is not complete");
         }
        TestAnalytics testAnalytics = reportGeneratorService.getTestAnalytics(userId, courseId);
+        Optional<CoursesDescription> coursesDescription = coursesDescriptionRepository.findById(reportOverall.getCourseId().getId());
+        if(!coursesDescription.isPresent()){
+            throw new Exception("Course/Test doesn't exist");
+        }
+        testAnalytics.setTotalMarks(coursesDescription.get().getTotalMarks());
         /*Set the User Rank*/
         List<ReportOverall> reportOverallList = reportGeneratorService.getRankWiseReport(courseId);
         Optional<ReportOverall> reportForRank = reportOverallList.stream().
